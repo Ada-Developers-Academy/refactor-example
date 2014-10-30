44a64b7bc83cb7d125729fa3b286fe4462795f6b
Bookis Smuin <bookis.smuin@gmail.com>
2014-10-30 11:06:08 -0700

    refactor order item totals

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,14 @@
 class Order < ActiveRecord::Base
   has_many :order_items
 
+  def total
+    sum = 0
+    order_items.all.each do |item|
+      sum += item.total
+    end
+    sum
+  end
+
   def self.all_by_date
     all.sort_by {|o| o.created_at }.reverse
   end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,16 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
+
+  def total
+    quantity * product.price
+  end
+
+  def self.total
+    sum = 0
+    self.all.each do |item|
+      sum += item.total
+    end
+    sum
+  end
 end
